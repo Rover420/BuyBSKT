44a854a263da8a2f36d585ae1b8e49b675797c3a
Rover420 <100524342+Rover420@users.noreply.github.com>
2022-09-13 00:32:34 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -69,78 +69,96 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>BSKT Wymiana</title>
-        <meta name="description" content="BSKT Wymiana" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <script src="https://rk64tcf9ny.eu-west-1.awsapprunner.com/widget/main.min.js" />
-        <script>var widget_id_6851681344231 = "aa044660-e663-444f-bc28-87e007c03e78"</script>
-        <meta charSet="utf-8" />
-      </Head>
+        <Head>
+            <title>BSKT Wymiana</title>
+            <meta name="description" content="BSKT Wymiana" />
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <script src="https://rk64tcf9ny.eu-west-1.awsapprunner.com/widget/main.min.js" />
+            <script>var widget_id_6851681344231 = "aa044660-e663-444f-bc28-87e007c03e78"</script>
+            <meta charSet="utf-8" />
+        </Head>
 
-      <div className={styles.main}>
-      <Navbar />
-      <div className={styles.wrapper}>
-      <h2>Wymiana</h2>
-            <div className={styles.panelwrapper}>
-                <div className={styles.Type}>
-                    <div style={{ width: '100%', background: '#1f1d39', color: '#fff' }} className={`${styles.option} ${styles.selected}`}>
-                        <span>Chcę kupić</span>
-                    </div>
-                </div>
-                <div className={styles.Currency}>
-                    <div className={`${styles.option} ${(currency === 'BSKT' || !currency) ? styles.selected : ''}`}
-                        onClick={() => {setCurrency('BSKT')}}>
-                        <span>BSKT</span>
-                        <div className={styles.imgwrapper}><Image src='/round_logo.png' height={22} width={22} priority></Image></div>
-                    </div>
-                    <div className={`${styles.option} ${currency === 'BNB' ? styles.selected : ''}`}
-                        onClick={() => {setCurrency('BNB')}}>
-                        <span>BNB</span>
-                        <div className={styles.imgwrapper}><Image src='/BNBIcon.png' height={22} width={22} priority></Image></div>
-                    </div>
-                    <div className={`${styles.option} ${currency === 'BUSD' ? styles.selected : ''}`}
-                        onClick={() => {setCurrency('BUSD')}}>
-                        <span>BUSD</span>
-                        <div className={styles.imgwrapper}><Image src='/BUSDIcon.png' height={22} width={22} priority></Image></div>
-                    </div>
-                    <div className={`${styles.option} ${currency === 'USDC' ? styles.selected : ''}`}
-                        onClick={() => {setCurrency('USDC')}}>
-                        <span>USDC</span>
-                        <div className={styles.imgwrapper}><Image src='/USDCIcon.png' height={22} width={22} priority></Image></div>
-                    </div>
-                </div>
-                <div className={styles.Offer}>
-                    <label htmlFor="cash">za</label>
-                    <input type="number" name="cash" id="cash" onChange={handleInput} defaultValue={500} />
-                    <div className={styles.fiat} onClick={() => {list ? setList(false) : setList(true)}}>
-                        <Image src={`/${fiat}Icon.svg`} width={22} height={22} />
-                        <span>{fiat ? fiat : 'USD'}</span>
-                        <Image src='/Arrow-down.svg' width={22} height={22} priority></Image>
-                        <div className={`${styles.list} ${list ? styles.active : ''}`}>
-                            {fiat !== 'PLN' ? <div className={styles.opt} onClick={() => {setFiat('PLN')}}>
-                                <Image src='/PLNIcon.svg' width={22} height={22} priority></Image>
-                                <span>PLN</span>
-                            </div> : ''}
-                            {fiat !== 'USD' ? <div className={styles.opt} onClick={() => {setFiat('USD')}}>
-                                <Image src='/USDIcon.svg' width={22} height={22} priority></Image>
-                                <span>USD</span>
-                            </div> : ''}
-                            {fiat !== 'EUR' ? <div className={styles.opt} onClick={(e) => {setFiat('EUR')}}>
-                                <Image src='/EURIcon.svg' width={22} height={22} priority></Image>
-                                <span>EUR</span>
-                            </div> : ''}
+        <div className={styles.main}>
+
+            <Navbar />
+
+            <div className={styles.wrapper}>
+
+                <div className={styles.container}>
+
+                <h2 className={styles.fadein}>Zakup <br /> <span style={{ fontWeight: 'normal', fontSize: '28px' }}>Blik / karta</span></h2>
+                <div className={styles.panelwrapper}>
+                    <div className={styles.Type}>
+                        <div className={`${styles.option} ${styles.selected}`}>
+                            <span>Kup</span>
+                        </div>
+                        <div className={`${styles.option} ${styles.disabled}`}>
+                            <span>Sprzedaj</span>
                         </div>
                     </div>
-                </div>
-                <div className={`${styles.Buy} ${disabled ? styles.disabled : ''}`} onClick={start}>Kup {value ? value.code === 'BNB' ? (value.amount).toFixed(8) : (value.amount).toFixed(2) : 0} {value ? value.code : 'BSKT'}</div>
+                    <div className={styles.Currency}>
+                        <div className={`${styles.option} ${(currency === 'BSKT' || !currency) ? styles.selected : ''}`}
+                            onClick={() => {setCurrency('BSKT')}}>
+                            <span>BSKT</span>
+                            <div className={styles.imgwrapper}><Image src='/round_logo.png' height={22} width={22} priority></Image></div>
+                        </div>
+                        <div className={`${styles.option} ${currency === 'BNB' ? styles.selected : ''}`}
+                            onClick={() => {setCurrency('BNB')}}>
+                            <span>BNB</span>
+                            <div className={styles.imgwrapper}><Image src='/BNBIcon.png' height={22} width={22} priority></Image></div>
+                        </div>
+                        <div className={`${styles.option} ${currency === 'BUSD' ? styles.selected : ''}`}
+                            onClick={() => {setCurrency('BUSD')}}>
+                            <span>BUSD</span>
+                            <div className={styles.imgwrapper}><Image src='/BUSDIcon.png' height={22} width={22} priority></Image></div>
+                        </div>
+                        <div className={`${styles.option} ${currency === 'USDC' ? styles.selected : ''}`}
+                            onClick={() => {setCurrency('USDC')}}>
+                            <span>USDC</span>
+                            <div className={styles.imgwrapper}><Image src='/USDCIcon.png' height={22} width={22} priority></Image></div>
+                        </div>
+                    </div>
+                    <div className={styles.Offer}>
+                        <label htmlFor="cash">za</label>
+                        <input type="number" name="cash" id="cash" onChange={handleInput} defaultValue={500} />
+                        <div className={styles.fiat} onClick={() => {list ? setList(false) : setList(true)}}>
+                            <Image src={`/${fiat}Icon.svg`} width={22} height={22} />
+                            <span>{fiat ? fiat : 'USD'}</span>
+                            <Image src='/Arrow-down.svg' width={22} height={22} priority></Image>
+                            <div className={`${styles.list} ${list ? styles.active : ''}`}>
+                                {fiat !== 'PLN' ? <div className={styles.opt} onClick={() => {setFiat('PLN')}}>
+                                    <Image src='/PLNIcon.svg' width={22} height={22} priority></Image>
+                                    <span>PLN</span>
+                                </div> : ''}
+                                {fiat !== 'USD' ? <div className={styles.opt} onClick={() => {setFiat('USD')}}>
+                                    <Image src='/USDIcon.svg' width={22} height={22} priority></Image>
+                                    <span>USD</span>
+                                </div> : ''}
+                                {fiat !== 'EUR' ? <div className={styles.opt} onClick={(e) => {setFiat('EUR')}}>
+                                    <Image src='/EURIcon.svg' width={22} height={22} priority></Image>
+                                    <span>EUR</span>
+                                </div> : ''}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`${styles.Buy} ${disabled ? styles.disabled : ''}`} onClick={start}>Kup {value ? value.code === 'BNB' ? (value.amount).toFixed(8) : (value.amount).toFixed(2) : 0} {value ? value.code : 'BSKT'}</div>
+                    </div></div>
+
+                    <div className={styles.container}>
+
+                    <h2 className={styles.fadein}>Jak to działa?</h2>
+                    <div className={`${styles.panelwrapper} ${styles.videowrapper}`}>
+                    <iframe style={{ borderRadius: '10px', width: '100%', height: '100%' }} title="BasketCoin - Jak to działa [PL] - 90 sek" src="https://www.youtube.com/embed/Tqa1M8KQyMo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+                    </div>
+
+                    </div>
+
             </div>
-            </div>
-          </div>
+        </div>
     </>
   )
 }
